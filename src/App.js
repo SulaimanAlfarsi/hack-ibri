1ceefad03817ba935057668ab635df4e0176a09f
Sulaiman Al-Farsi <132258182+SulaimanAlfarsi@users.noreply.github.com>
2025-04-22 16:00:58 +0400

    install tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-black' >
+      <h1 className='text-7xl text-center text-blue-400 '>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
